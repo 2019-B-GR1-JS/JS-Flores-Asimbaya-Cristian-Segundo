a308d5d1a21c4c67973d6d8dd126152b464a2148
Cristian Flores <kristian192019d@gmail.com>
2019-11-18 16:57:31 -0500

    Clase 18 de nov

--- 01- Javascript/06-sync-async.js
+++ 01- Javascript/06-sync-async.js
@@ -81,24 +81,76 @@ console.log('02) Terminar')
 
 //console.log('finalizar.');
 
-const leerArchivo = (pathArchivo) => {
-    return new Promise(
-        (resolve, reject) => {
-            // res -> resolve rej -> reject
-
+    const leerArchivo = (pathArchivo)=>{
+        return new Promise(
+            (resolve,reject)=>{
+                //res, rej
                 fs.readFile(
-                pathArchivo,
-                'utf8',
-                (error, archivoLeido) => {
-                    if (error){
-                        reject(error);
-                    } else {
-                        resolve(archivoLeido);
-                    }
-                }
-                );
+                    pathArchivo,
+                    'utf8',
+                    (error, archivo) => {
+                        if(error){
+                            reject(error);
+                        }else {
+                            resolve(archivo);
+                        }
 
-        }
-    )
+                    }
+                )
+            }
+        )
+    }
+
+    leerArchivo('./01-variables.js')
+        .then(
+            (contendoCallbackjs)=>{
+            //    console.log(contendoCallbackjs);
+                return leerArchivo('./01-variables.js') //promesa
+            }
+        )
+        .then(
+            (contenido2js)=>{
+            //    console.log(contenido2js);
+            }
+        )
+        .catch(
+            (error)=>{
+             //   console.error('Error JS', error);
+            }
+        )
+
+
+const nombreArchivo = './05-callbacks.js';
+
+//Cuando se use codigo sincrono siempre hay que usar un try y un catch
+
+console.log('Inicia sincrono');
+
+try {
+    const contenidoArchivo = fs.readFileSync(nombreArchivo,'utf-8');
+    console.log(contenidoArchivo);
+    console.log('Se leyo correctamente');
+} catch (e) {
+    console.error('Error', error);
 }
 
+console.log('Termina sincrono');
+
+//Transformar una promesa a codigo sincrono
+// una Funcion, Funtion Anonymous
+
+// paso 1: poner la palabra ASYNC permite usar codigo sincrono dentro de la funcion
+// paso 2: para trasformar promesa -> sincrono usar la palabra "await"
+async function leerArchivoSync() {
+
+    try {
+        const contenido = await leerArchivo(nombreArchivo); // Devuelve una promesa,
+        console.log(contenido);
+        console.log("Leimos con async y await");
+    }catch (e) {
+        console.log('Error', error);
+    }
+
+}
+
+//Toda funcion que se usa con un async, se transforma en una promesa
